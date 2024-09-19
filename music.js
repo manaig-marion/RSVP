f721c28d08d300f6a7e09afc441e45e39a5cb5f4
Marion Manaig <mariondavemanaig@gmail.com>
2024-09-20 00:44:23 +0800

    final

--- music.js
+++ music.js
@@ -10,12 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
         if (count === 0) {
             count = 1;
             audio.play();
-            playButton.textContent = 'Click To Pause Music';
         } else {
             count = 0;
             audio.pause();
             audio.currentTime = 0;
-            playButton.textContent = 'Click To Play Music';
         }
     }
 
